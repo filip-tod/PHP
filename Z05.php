21b025edf5c3c7cfea8543b0cdd842452c89a378
Filip Todorovic <filip.tod95@gmail.com>
2022-06-29 11:27:39 +0200

    .

---
+++ Z05.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- START zagljavlje -->
+    <header>
+        <?php require_once 'zaglavlje.php'; ?>
+    </header>
+    <!-- END zaglavlje -->
+
+    <!-- START tijelo -->
+    <main>
+        <div class="callout">
+            <div class="gornji-tekst">
+                <h5>Stranica prima 3 GET parametra (x, y, z) koji su cijeli brojevi
+                    te ispisuje najveći primljeni broj
+                </h5>
+            </div>
+        </div>
+        <div class="callout" id="izlaz">
+            <?php
+            $p1= isset($_GET['p2']) ? $_GET['p1'] : 0;
+            $p2 = isset($_GET['p2']) ? $_GET['p2'] : 0;
+            $p3 = isset($_GET['p3']) ? $_GET['p3'] : 0;
+            ?>
+
+            <!-- <ul>
+                <li>Prvi parametar: <?php echo $_GET['p1']; ?></li>
+                <li>Drugi parametar: <?php echo $_GET['p2']; ?></li>
+                <li>Treći parametar: <?php echo $_GET['p3']; ?></li>
+            </ul> -->
+
+            <div>
+                <b>Najveći broj: </b>
+                <?php
+               if (isset($_GET['p1']) && isset($_GET['p2']) && isset($_GET['p3'])) {
+                    if ($p1 > $p2 && $p1 > $p3) {
+                        echo $p1;
+                    } else if ($p2 > $p1 && $p2 > $p3) {
+                        echo $p2;
+                    } else if ($p3 > $p1 && $p3 > $p2) {
+                        echo $p3;
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </main>
+    <!-- END tijelo -->
+
+    <!-- START podnožje -->
+    <footer>
+        <?php require_once 'podnozje.php'; ?>
+    </footer>
+    <!-- END podnožje -->
+
+
+    <!-- JavaScript -->
+    <?php require_once 'js.php'; ?>
+</body>
+
+</html>
---
+++ Z06.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en" dir="ltr">
+
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Z06</title>
+</head>
+
+<body>
+    <!-- START zagljavlje -->
+    <header>
+        <?php require_once 'zaglavlje.php'; ?>
+    </header>
+    <!-- END zaglavlje -->
+
+    <!-- START tijelo -->
+    <main>
+        <div class="callout">
+            <div class="gornji-tekst">
+                <h5>Stranica prima cijeli broj (i).
+                    Ako je broj paran boja okvira treba biti zelena,
+                    inače treba biti crvena.
+                </h5>
+            </div>
+        </div>
+        <?php
+        $i = $_GET['i'];
+
+        if (isset($_GET['i'])) {
+            if ($i % 2 === 0) {
+                echo '<div style=\'background-color: green;\'>paran broj.</div>';
+            } else if ($i % 2 === 1) {
+                echo '<div style=\'background-color: red;\'>Neparan broj.</div>';
+            }
+        } else echo '<b>Unesi parametar!</b>';
+
+        ?>
+    </main>
+    <!-- END tijelo -->
+
+    <!-- START podnožje -->
+    <footer>
+        <?php require_once 'podnozje.php'; ?>
+    </footer>
+    <!-- END podnožje -->
+
+
+    <!-- JavaScript -->
+    <?php require_once 'js.php'; ?>
+</body>
+
+</html>
+<!-- 
+
+// Stranica prima cijeli broj.
+// ako je broj paran boja stranice treba biti zelena
+// inač treba biti crvena -->
