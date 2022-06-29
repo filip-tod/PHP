68c94f6fc820729826d506148f6cbe78f28d7f1e
Filip Todorovic <filip.tod95@gmail.com>
2022-06-29 13:14:52 +0200

    Create Z04.php

---
+++ Z04.php
@@ -0,0 +1,20 @@
+<?php
+            $a = $_GET['a'];
+            $b = $_GET['b'];
+            $c = $_GET['c'];
+            $d = $_GET['d'];
+            $umnozak1 = $_GET['a'] * $_GET['b'];
+            $umnozak2 = $_GET['c'] * $_GET['d']; 
+            $rezultat = $umnozak1 * $umnozak2;
+            echo $rezultat;
+            ?>
+<!-- 
+
+// Stranica prima 4 GET parametara i ispisuje 
+// umnožak zbroja prvi i drugi te treći u četvrti
+
+// primjer ulaza
+// ulaz: 2 2 1 3
+// izlaz: 16 -->
+
+
